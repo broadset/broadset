159e3eab294a3cae055d4d7b99615d2d7cda7e36
Timo Korkalainen <timo@timokorkalainen.net>
2026-04-13 08:52:01 +0300

    fix: define path-bool env for browser

--- packages/demo/vite.config.ts
+++ packages/demo/vite.config.ts
@@ -4,5 +4,8 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  define: {
+    'process.env.PATH_BOOL_DEV_ASSERTS': JSON.stringify('0'),
+  },
   plugins: [tailwindcss(), react()],
 });
