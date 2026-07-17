5976d2f46537acd50aae33963c790268854bd7db
Timo Korkalainen <timo@timokorkalainen.net>
2026-07-18 01:22:42 +0300

    feat(ui): timeline track lanes with categorized draggable markers

--- packages/ui/src/timeline/index.ts
+++ packages/ui/src/timeline/index.ts
@@ -1,5 +1,6 @@
 export * from './keyframe-authoring-panel';
 export * from './timeline-context';
+export * from './timeline-lanes';
 export * from './timeline-math';
 export * from './timeline-ruler';
 export * from './timeline-types';
